93426002257ae07f82fd8d9017119036c764af5b
Pavel Pelovski <git.pelovski@gmail.com>
2022-12-03 13:22:56 +0100

    separete logic into diffrent files

--- 04. JS-Applications-Single-Page-Applications-Exercise-Resources/02.Movies/app.js
+++ 04. JS-Applications-Single-Page-Applications-Exercise-Resources/02.Movies/app.js
@@ -1,1 +1,40 @@
-console.log("it works!");
+import { homePage } from "./home.js";
+import { loginPage } from "./login.js"
+import { registerPage } from "./register.js";
+import { createPage } from "./create.js";
+import { detailsPage } from "./details.js";
+import { editPage } from "./edit.js";
+
+const routers = {
+    '/': homePage,
+    '/login': loginPage,
+    '/logout': logoutPage,
+    '/register': registerPage,
+    '/create': createPage
+};
+
+document.querySelector('nav').addEventListener('click', onNavigate);
+document.querySelector('#add-movie-button a').addEventListener('click', onNavigate);
+
+function onNavigate(e) {
+
+    if (e.target.tagName == 'A' && e.target.href) {
+        e.preventDefault();
+        const url = new URL(e.target.href);
+
+        const view = routers[url.pathname];
+
+        if (typeof view == 'function') {
+
+            view();
+        }
+    }
+}
+
+
+function logoutPage() {
+    alert('Logged out');
+}
+
+// Start App in catalog view
+homePage()
--- 04. JS-Applications-Single-Page-Applications-Exercise-Resources/02.Movies/util.js
+++ 04. JS-Applications-Single-Page-Applications-Exercise-Resources/02.Movies/util.js
@@ -1,1 +1,11 @@
- 
+const views =  [...document.querySelectorAll('.view-section')];
+
+function hideAll(){
+    views.forEach(x => x.style.display = 'none');
+
+}
+
+export function showView(section){
+    hideAll();
+    section.style.display = 'block';
+}
