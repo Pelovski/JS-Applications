b55c7f3e8505eafc7a7a8c88682016a718f4d051
Pavel Pelovski <git.pelovski@gmail.com>
2022-12-05 10:51:20 +0100

    Ex.4 completed

--- 05. JS-Applications-Client-Side-Rendering-Exercise-Resources/04.Fill-Dropdown/dropdown.js
+++ 05. JS-Applications-Client-Side-Rendering-Exercise-Resources/04.Fill-Dropdown/dropdown.js
@@ -1,3 +1,37 @@
-function addItem() {
-    console.log('TODO:...');
+import { html, render } from '../node_modules/lit-html/lit-html.js';
+
+const  baseUrl = 'http://localhost:3030/jsonstore/advanced/dropdown';
+const itemInputText = document.getElementById('itemText');
+const optinalElement = document.getElementById('menu');
+
+document.querySelectorAll('input')[1].addEventListener('click', addItem);
+
+
+const itemsTemplate = (items) => html `
+    ${items.map(item => html `
+        <option value=${item._id}>${item.text}</option>
+    `)};
+`;
+
+takeItems();
+async function takeItems(){
+    const res = await fetch(baseUrl);
+    const items = Object.values( await res.json() );
+
+    console.log(items.map(x => x));
+    render(itemsTemplate(items),optinalElement);
+}
+
+async function addItem(e) {
+    e.preventDefault();
+
+    const res = await fetch(baseUrl, {
+        method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ "text": itemInputText.value })
+    });
+    itemInputText.value = '';
+    takeItems();
 }
